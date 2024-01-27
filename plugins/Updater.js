c5327348c852bfc845ec10946b1b2735796c15cf
Astropeda <152017512+Astropeda@users.noreply.github.com>
2024-01-27 20:57:27 +0100

    Updater Adjusted

--- plugins/Updater.js
+++ plugins/Updater.js
@@ -1,14 +1,9 @@
-
-
 const DB = require('../lib/scraper')
 const { tlang, name, prefix, Module_Exports } = require('../lib')
 const simpleGit = require('simple-git');
 const git = simpleGit();
 const Heroku = require('heroku-client');
 //---------------------------------------------------------------------------
-
- 
-
 
 async function updateHerokuApp() {
     const heroku = new Heroku({ token: process.env.HEROKU_API_KEY });
@@ -22,13 +17,11 @@ async function updateHerokuApp() {
       await git.push('heroku', 'main');
       return "*_Bot Updated SuccessFully_*\n*_Wait While Restarting_*";
     }
-  }
-
-  
+  } 
 //---------------------------------------------------------------------------
 Module_Exports({
-            kingcmd: "update",
-            shortcut: ["ud"],
+            kingcmd: "checkupdate",
+            shortcut: ["ud", "update"],
             infocmd: "Shows repo\'s refreshed commits.",
             kingclass: "tools",
             kingpath: __filename
@@ -63,8 +56,8 @@ if(name.HEROKU_APP_NAME && name.HEROKU_API_KEY )
 {
         
      Module_Exports({
-                 kingcmd: "updatebot",
-                 shortcut: ["ubot"],
+                 kingcmd: "updatenow",
+                 shortcut: ["ubot", "updateall", "updatebot"],
                  infocmd: "Shows repo\'s refreshed commits.",
                  kingclass: "tools",
                  kingpath: __filename
@@ -76,8 +69,8 @@ if(name.HEROKU_APP_NAME && name.HEROKU_API_KEY )
                 let update = await DB.sync()
                 let buttonMessaged = 
                 {
-                     text:" ♕ *Updating Your Bot...!*\n  *★────────────────────────★*\n"+update +"\n  *★────────────────────────★*",
-                     footer: 'UPDATER --- sᴜʜᴀɪʟ ᴛᴇᴄʜ ɪɴғᴏ \n www.youtube.com/c/Astropeda',
+                     text:" ⬇️ *Updating Your Bot...!",
+                     footer: 'UPDATER --- sᴜʜᴀɪʟ ᴛᴇᴄʜ ɪɴғᴏ \nAstropeda',
                      headerType: 4,
                 };
                 await Void.sendMessage(citel.chat, buttonMessaged);
@@ -87,66 +80,3 @@ if(name.HEROKU_APP_NAME && name.HEROKU_API_KEY )
                 process.exit(0);
        })
 }
-/*
-Module_Exports({
-    kingcmd: "update all",
-    infocmd: "Shows repo's refreshed commits.",
-    kingclass: "tools",
-    kingpath: __filename
-},
-async(Void, citel, text,{ isCreator }) => {
-    await git.fetch();
-    var commits = await git.log(['main' + '..origin/' + 'main']);
-    if (commits.total === 0) {
-        return await citel.reply('Bot is UP-TO-DATE')
- }
-// if (!isHeroku){
-   // await require("simple-git")().reset("hard",["HEAD"])
-   // await require("simple-git")().pull()
-   // await citel.reply("_Successfully updated. Please manually update npm modules if applicable!_")
-   // process.exit(0);    
-   // }
-    //else if (isHeroku) {
-       // await fixHerokuAppName(message)
-        await citel.reply('Starting Update...')
-
- try { var app = await heroku.get('/apps/' + Config.HEROKU_APP_NAME)  }
- catch { await citel.reply('Heroku Information Wrong')
-        await new Promise(r => setTimeout(r, 1000)); }
- 
-        git.fetch('upstream', 'main');
-        git.reset('hard', ['FETCH_HEAD']);
-        var git_url = app.git_url.replace( "https://", "https://api:" + Config.HEROKU_API_KEY + "@"  )
-        try { await git.addRemote('heroku', git_url);  } 
-       catch { console.log('null '); }
-        await git.push('heroku', 'main');
-       await citel.reply(`${name.botname} *_Updated Successfully_*`)
-       await citel.reply("_Restarting_")
-        } else {
-            await update("UPDATER",'default')
-            await citel.reply("_Starting Update!_")
-    }
- 
-}
-)
-
-//______________________________________________________________\\
-async function fixHerokuAppName(message){
-    if (!HEROKU_API_KEY) return await message.sendReply(`_You have not provided HEROKU_API_KEY\n\nPlease fill this var, get api key from heroku account settings_`)
-    let apps = await heroku.get('/apps')
-    let app_names = apps.map(e=>e.name)
-    if (!HEROKU_APP_NAME || !app_names.includes(Config.HEROKU_APP_NAME)){
-    function findGreatestNumber(e){let t=e[0];for(let n=1;n<e.length;n++)e[n]>t&&(t=e[n]);return t}
-    let times = apps.map(e=>new Date(e.updated_at).getTime())
-    let latest = findGreatestNumber(times)
-    let index = times.indexOf(latest)
-    let app_name = apps[index].name
-    Config.HEROKU_APP_NAME = app_name
-    process.env.HEROKU_APP_NAME = app_name
-    baseURI = '/apps/' + app_name;
-    await message.sendReply(`_You provided an incorrect heroku app name, and I have corrected your app name to "${app_name}"_\n\n_Please retry this command after restart!_`)    
-    Config.HEROKU_APP_NAME = app_name
-        return await setVar("HEROKU_APP_NAME",app_name,message)
-    }
-}
-*/
