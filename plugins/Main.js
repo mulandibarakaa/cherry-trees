21b255365120712dd3e3668d56680498a45c0141
Astropeda <astromedia0010@outlook.com>
2024-01-28 18:15:52 +0100

    Asta-Md

--- plugins/Main.js
+++ plugins/Main.js
@@ -121,6 +121,15 @@ Module_Exports({
           forwardingScore: 999,
           isForwarded: true,
         },
+        externalAdReply: {
+          title: "ᴀꜱᴛᴀ ᴍᴅ 2024",
+          body: "ꜰᴏʟʟᴏᴡ ᴜꜱ",
+          thumbnailUrl: "",
+          thumbnail: log0,
+          mediaType: 4,
+          mediaUrl: "",
+          sourceUrl: sgc
+        },
         image: {
           url: await botpic()
         },
