c4cfe90f2c323a81e8a50f5dcf0bdd1a4169ecf2
mulandibarakaa <128398331+mulandibarakaa@users.noreply.github.com>
2024-01-31 09:37:36 +0300

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '2348039607375' ;
+global.owner = process.env.OWNER_NUMBER || '254768827492' ;
 global.mongodb = process.env.MONGODB_URI || "" ; 
 global.port=5000  ;
 global.audio = '' ; 
@@ -8,25 +8,25 @@ global.video = '' ;
 global.blockJids = process.env.BLOCK_JID ||'120363169665426586@g.us' ;
 global.allowJids = process.env.ALLOW_JID ||'120363169665426586@g.us' ;
 global.email = 'astromedia0010@outlook.com' ; 
-global.github = 'https://github.com/Astropeda/Asta-Md' ;
-global.location = 'Nigeria Lagos' ;
-global.timezone  = process.env.TIME_ZONE || 'Africa/Lagos'
+global.github = 'https://github.com/mulandibarakaa/cherry-trees' ;
+global.location = 'kenya Nairobi' ;
+global.timezone  = process.env.TIME_ZONE || 'Africa/Nairobi'
 global.gurl = '' ; 
-global.sudo =  process.env.SUDO || "2348039607375" ;
-global.devs = "2348039607375"; //Dont change it
-global.mztit = process.env.MZTIT ||"Asta-Md", 
-global.Gname = process.env.GNAME ||"Asta-Md-sᴜᴘᴘᴏʀᴛ",
+global.sudo =  process.env.SUDO || "254768827492" ;
+global.devs = "254768827492"; //Dont change it
+global.mztit = process.env.MZTIT ||"cherry-md", 
+global.Gname = process.env.GNAME ||"undefined",
 global.zyt = process.env.ZYT || '1',
 global.waUrl = process.env.WAURL ||"",
 global.website = '' ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' ;
 module.exports = {
   sessionName: process.env.SESSION_ID || '',
-  botname: process.env.BOT_NAME || 'Asta-Md',
-  ownername:  process.env.OWNER_NAME || `Astro`,
-  author:  process.env.PACK_AUTHER || 'Astro', 
+  botname: process.env.BOT_NAME || 'cherry-Md',
+  ownername:  process.env.OWNER_NAME || `Ace`,
+  author:  process.env.PACK_AUTHER || 'Ace', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
-  packname:  process.env.PACK_NAME || "Asta-Md" , 
+  packname:  process.env.PACK_NAME || "cherry trees" , 
   autoreaction: process.env.AUTO_REACTION || 'true',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'true', 
@@ -44,7 +44,7 @@ module.exports = {
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "34wcCGPoe3yaGtpiBZgx4SN7",
-  caption :process.env.CAPTION || "\t*ᴘᴏᴡᴇʀᴇᴅ ʙʏ Asta-Md* ",
+  caption :process.env.CAPTION || "\t*wavy cherry trees* ",
   promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'true' ,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' ,
   VERSION: process.env.VERSION || 'V.4.5.0',
